ea56e1c911a94d2bddb1bc09fda798c2131e268f
Sanic00 <wodus48@gmail.com>
2023-09-19 16:37:35 +0900

    'return-hw'

--- Js/return.js
+++ Js/return.js
@@ -15,7 +15,7 @@ function na() { // 함수는 수학이랑 똑같음
 
 var 리턴 = na();
 console.log(리턴);// 실행하면 123을 퇴하고 뱉는다 (리턴으로 반환 함)
-
+  
 //함수는 마법의 박스 같은거임
 
 function vat(price) {
@@ -34,4 +34,26 @@ var num = (a * 1.1).toFixed(2); //숫자.toFixed(몇자리) <- 이거 쓰면 문
 //자바스크립트 + 연산자 특징
 // '1' + 1  -> '11'로 됨 문자특징임
 // 문자 + 문자 -> 문자 
-// 그러기 떄문에 
+// 그러기 떄문에 '숫자'를 숫자로 변환하고 싶으면 
+//parseFloat('123') 실수
+//parseInt('123')  정수
+
+//Q1. 함수에 분과 초를 차례로 파라미터로 입력하면 ms단위로 바꿔서 뱉어주는 함수를 만들어봅시다.
+//(1초 == 1000ms 입니다)
+           
+function ms(min, sec) {
+  return (min * 60 + sec) * 1000 ; 
+}
+
+ms(2, 10);
+
+function price(a, b){
+    var result = a * 0.9;
+    if(b == true){
+        var result= result - 1.5 ;
+    }
+        return result ; 
+    }
+
+
+price(10, true);
