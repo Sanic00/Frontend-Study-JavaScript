659e0e006ab39bc5cd712337d82685ba71d9d6c3
Sanic00 <wodus48@gmail.com>
2023-09-21 11:32:26 +0900

    'tap;

--- Js/for.js
+++ Js/for.js
@@ -6,26 +6,22 @@
 // 대신 모든버튼에 붙은 orange 클래스 명 제거  div박스 show 클래스명제거도 해야됨
 // eq(0) == qeurySelectorAll('tab-button')[0] 이랑 똑같음
 $('.tab-button').eq(0).on('click', function () {
+    $('.tab-button').removeClass('orange');
     $('.tab-button').eq(0).addClass('orange');
-    $('.tab-button').eq(1, 2).removeClass('orange');
+    $('.tab-content').removeClass('show');
     $('.tab-content').eq(0).addClass('show'); 
-    $('.tab-content').eq(1 , 2).removeClass('show');
 });
 
 $('.tab-button').eq(1).on('click', function () {
+    $('.tab-button').removeClass('orange');
     $('.tab-button').eq(1).addClass('orange');
-    $('.tab-button').eq(0).removeClass('orange');
-    $('.tab-button').eq(2).removeClass('orange');
+    $('.tab-content').removeClass('show');
     $('.tab-content').eq(1).addClass('show'); 
-    $('.tab-content').eq(0).removeClass('show');
-    $('.tab-content').eq(2).removeClass('show');
 });
 
 $('.tab-button').eq(2).on('click', function () {
+    $('.tab-button').removeClass('orange');
     $('.tab-button').eq(2).addClass('orange');
-    $('.tab-button').eq(0).removeClass('orange');
-    $('.tab-button').eq(1).removeClass('orange');
+    $('.tab-content').removeClass('show');
     $('.tab-content').eq(2).addClass('show'); 
-    $('.tab-content').eq(0).removeClass('show');
-    $('.tab-content').eq(1).removeClass('show');
 });
